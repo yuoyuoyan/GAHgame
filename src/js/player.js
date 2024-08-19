0962bf32b47da4a40742b18f4fe1cf38871a1cc3
qi.wang <qi.wang@bytedance.com>
2024-08-19 15:15:14 +0800

    Add standard hotel and recording config, untested

--- src/js/player.js
+++ src/js/player.js
@@ -1,8 +1,9 @@
 // Player class definition
 class Player{
-    constructor(game, playerID, playerName, hotelID) {
+    constructor(game, player, playerID, playerName, hotelID) {
         // init player basic info
         this.game = game;
+        this.player = player;
         this.playerID = playerID;
         this.playerName = playerName;
         console.log("create new player " + this.playerID + " " + this.playerName);
@@ -81,7 +82,7 @@ class Player{
         this.serverOnHandHighLight = [];
         this.serverHiredHighLightFlag = false;
         this.serverHiredHighLight = [];
-        this.hotel = new Hotel(this.game, hotelID); // prepare hotel
+        this.hotel = new Hotel(this.game, this.player, hotelID); // prepare hotel
         // prepare the first three rooms
         this.hotel.highlightRoomToPrepare(this.money);
         this.hotel.highlightRoomToPrepare(this.money);
